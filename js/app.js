12cfbc242ebdb585214bf56a3a1f0bfa7f2a437f
Asim <asim.mahar@gmail.com>
2015-08-28 19:12:20 -0400

    added user random number

--- js/app.js
+++ js/app.js
@@ -2,18 +2,29 @@
 
 $('.document').ready(function(){
 
-for (var i=1; i < 100; i++){
+//Validate and enter user input
+	var validNumber = '';
+	var number = prompt("Please kindly enter a number");
 
-	var message = i;
-
-	if(i%3 == 0 && i%5 == 0){
-		message = "FizzBuzz";
-	} else if(i%5 == 0){
-		message = "Buzz";
-	} else if (i%3 == 0){
-		message = "Fizz";
+	if (isNaN(number)){
+		newNumber = prompt("please enter a valid number");
+		validNumber = +newNumber;
+	} else{
+		validNumber = +number
 	}
-	$('.output').append('\n' + message + '\n');	
-
-};
+//run the main function
+	function userInput (validNumber){
+		for (var i=1; i <= validNumber; i++){
+			var message = i;
+			if(i%3 == 0 && i%5 == 0){
+				message = "FizzBuzz";
+			} else if(i%5 == 0){
+				message = "Buzz";
+			} else if (i%3 == 0){
+				message = "Fizz";
+			}
+			$('.output').append('\n' + message + '\n');	
+	    };
+	};
+	userInput(validNumber);
 });
